45bf11e5b7555bd29fb07258900d7bebdaf8e3c2
Tasin Mir <tasinmir20@gmail.com>
2022-06-02 23:56:25 +0600

    Create a Card - api

--- api/controllers/space/board.js
+++ api/controllers/space/board.js
@@ -72,7 +72,10 @@ exports.getList = async (req, res, next) => {
 					let getLists = await List.find({ spaceRef: spaceId }).sort({ createdAt: -1 }).select("name").skip(skip).limit(limit);
 					getLists = JSON.parse(JSON.stringify(getLists));
 					for (const list of getLists) {
-						const getCards = await Card.find({ listRef: list._id }).select("name description tags startDate endDate");
+						const getCards = await Card.find({ listRef: list._id }).select("name description progress tags startDate endDate").populate({
+							path: "tags",
+							select: "name color",
+						});
 						list.cards = getCards;
 					}
 					res.json({ lists: getLists });
@@ -84,6 +87,75 @@ exports.getList = async (req, res, next) => {
 			}
 		} else {
 			issue.message = "Invalid space id!";
+		}
+
+		if (!res.headersSent) {
+			res.status(400).json({ issue });
+		}
+	} catch (err) {
+		next(err);
+	}
+};
+
+exports.createCard = async (req, res, next) => {
+	let { spaceId, listId } = req.params;
+	let { name } = req.body;
+	try {
+		const user = req.user;
+		const issue = {};
+
+		if (name) {
+			name = String(name)
+				.replace(/\r\n/g, " ")
+				.replace(/[\r\n]/g, " ")
+				.replace(/  +/g, " ")
+				.trim();
+
+			const isValidSpaceId = isValidObjectId(spaceId);
+			const isValidListId = isValidObjectId(listId);
+			if (isValidSpaceId && isValidListId) {
+				const existsSpace = await Space.exists({ _id: spaceId });
+				if (existsSpace) {
+					const doIHaveAccess = await Space.exists({ $and: [{ _id: spaceId }, { "members.member": user._id }] });
+					if (doIHaveAccess) {
+						const existsList = await List.exists({ _id: listId });
+						if (existsList) {
+							const isListOfTheSpace = await List.exists({ $and: [{ _id: listId }, { spaceRef: spaceId }] });
+							if (isListOfTheSpace) {
+								const isDuplicate = await Card.exists({ $and: [{ listRef: listId }, { name: new RegExp(`^${name}$`, "i") }] });
+								if (!isDuplicate) {
+									const cardStructure = new Card({
+										name,
+										spaceRef: spaceId,
+										listRef: listId,
+										creator: user._id,
+									});
+									const createCard = await cardStructure.save();
+									res.status(201).json({ card: createCard });
+								} else {
+									issue.message = "Couldn't create a card with duplicate name in the same list!";
+								}
+							} else {
+								issue.message = "Something is wrong!";
+							}
+						} else {
+							issue.message = "Not found the list!";
+						}
+					} else {
+						issue.message = "You have no access to this space!";
+					}
+				} else {
+					issue.message = "Not found space!";
+				}
+			} else {
+				if (!isValidSpaceId) {
+					issue.message = "Invalid space id!";
+				} else if (!isValidListId) {
+					issue.message = "Invalid list id!";
+				}
+			}
+		} else {
+			issue.message = "Please provide a name to create a card!";
 		}
 
 		if (!res.headersSent) {
--- api/routes/space/board.js
+++ api/routes/space/board.js
@@ -1,8 +1,9 @@
 const boardRouter = require("express").Router({ mergeParams: true });
 
-const { createList, getList } = require("../../controllers/space/board");
+const { createList, getList, createCard } = require("../../controllers/space/board");
 
 boardRouter.post("/", createList);
 boardRouter.get("/", getList);
+boardRouter.post("/:listId/card", createCard);
 
 module.exports = boardRouter;
