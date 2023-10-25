aa0fae0a07d058dbd4b8961235f80b3214868532
antor-arif <rahmanarifur4838@gmail.com>
2023-10-25 21:27:41 +0600

    Early user  email added

--- api/routes/admin/index.js
+++ api/routes/admin/index.js
@@ -4,11 +4,13 @@ const multipart = require("connect-multiparty");
 
 const { getAdminList, getAdminProfile, getSingleAdminData, updateAdminData } = require("../../controllers/admin");
 const userRoutes = require("./user");
+const { getAllEmails } = require("../../controllers/email");
 
 adminRouter.use("/users", userRoutes);
 adminRouter.get("/", getAdminList);
 adminRouter.get("/profile", getAdminProfile);
 adminRouter.patch("/profile", multipart(), updateAdminData);
 adminRouter.get("/:adminId", getSingleAdminData);
+adminRouter.get("/get-early-emails", getAllEmails);
 
 module.exports = adminRouter;
