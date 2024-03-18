2bea63b8c0d377334a4ecca81b275957b7a46794
antor-arif <arifurrahman.ar10@gmail.com>
2024-03-19 00:08:32 +0600

    cloudinary configured successfully

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
+require("dotenv").config();
 
 function databaseConnection() {
 	const { DB_URI_CLOUD, DB_URI_LOCAL, NODE_ENV } = process.env;
 
-	const URL = NODE_ENV === "production" ? DB_URI_CLOUD : DB_URI_LOCAL;
+	const URL = NODE_ENV === "production" ? DB_URI_CLOUD : DB_URI_CLOUD;
 
 	mongoose
 		.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
 		.then(() => {
-			console.log(`Database connected to ${NODE_ENV === "production" ? "Cloud" : "Local"}`);
+			console.log(`Database connected to ${NODE_ENV === "production" ? "Cloud" : "Cloud"}`);
 		})
 		.catch((e) => {
 			return console.log(e);
